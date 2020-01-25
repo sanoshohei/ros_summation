de6bf2f15a568df3349e44ba19ead081ad2306cb
sanoshohei <38240672+sanoshohei@users.noreply.github.com>
2020-01-25 20:21:21 +0900

    Update summation.py

--- scripts/summation.py
+++ scripts/summation.py
@@ -5,22 +5,27 @@ from std_msgs.msg import Float64
 summation = 0
 c = 0
 
+pub = rospy.Publisher('sum', Float64, queue_size=1)
+
 def cb(message):
     # kahan summation
     global summation
     global c
+
+    global pub
     
     y = message.data - c
     t = summation + y
     c = (t - summation) - y
     summation = t
 
+    pub.publish(summation)
+    rospy.loginfo('publish {}'.format(summation))
+
+
 if __name__ == '__main__':
     rospy.init_node('power')
     sub = rospy.Subscriber('count_up', Float64, cb)
-    pub = rospy.Publisher('power', Float64, queue_size=1)
     rate = rospy.Rate(10)
-    while not rospy.is_shutdown():
-        rate.sleep()
-        pub.publish(summation)
-        rospy.loginfo('publish {}'.format(summation))
+
+    rospy.spin()
